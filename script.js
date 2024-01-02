125ff827187f83dc0d456fc9f236814f2297b911
AlirezaRa94 <alirezaraei17@gmail.com>
2024-01-02 11:42:04 +1100

    refactor: add formatMovementDate function
    - Implemented formatMovementDate function to display movement dates
    - Movement dates are displayed as 'Today', 'Yesterday', 'X days ago'
    - Added functionality to formatMovementDate for showing the actual date for entries older than a week

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-07-26T17:01:17.194Z',
-    '2020-07-28T23:36:17.929Z',
-    '2020-08-01T10:51:36.790Z',
+    '2023-11-18T21:31:17.178Z',
+    '2023-12-23T07:42:02.383Z',
+    '2024-01-01T09:15:04.904Z',
+    '2024-01-01T10:17:24.185Z',
+    '2024-01-02T14:11:59.604Z',
+    '2024-01-02T17:01:17.194Z',
+    '2024-01-02T23:36:17.929Z',
+    '2024-01-03T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -28,14 +28,14 @@ const account2 = {
   pin: 2222,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2023-11-01T13:15:33.035Z',
+    '2023-11-30T09:48:16.867Z',
+    '2023-12-25T06:04:23.907Z',
+    '2023-12-28T14:18:46.235Z',
+    '2024-01-01T16:33:06.386Z',
+    '2024-01-02T14:43:26.374Z',
+    '2024-01-02T18:49:59.371Z',
+    '2024-01-03T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -69,6 +69,20 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs((date2 - date1) / (24 * 60 * 60 * 1000)));
+
+  const targetDate = new Date(date);
+  const daysPassed = calcDaysPassed(new Date(), targetDate);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  return targetDate.toDateString();
+};
+
 const displayMovements = function (acc, sorted = false) {
   // Empty the container
   containerMovements.innerHTML = '';
@@ -81,14 +95,15 @@ const displayMovements = function (acc, sorted = false) {
   // Display movements
   movs.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
-    const displayDate = new Date(acc.movementsDates[i]).toDateString();
 
     const html = `
         <div class="movements__row">
           <div class="movements__type movements__type--${type}">
             ${i + 1} ${type}
           </div>
-          <div class="movements__date">${displayDate}</div>
+          <div class="movements__date">${formatMovementDate(
+            acc.movementsDates[i]
+          )}</div>
           <div class="movements__value">${mov.toFixed(2)}€</div>
         </div>
         `;
